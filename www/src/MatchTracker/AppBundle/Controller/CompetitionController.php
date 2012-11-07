b0f5a47667e432f5f0f65174d3a2acb50cfe0da1
bert.beeckman <bert.beeckman@kahosl.be>
2012-11-07 02:55:41 +0100

    Update competition maker

--- www/src/MatchTracker/AppBundle/Controller/CompetitionController.php
+++ www/src/MatchTracker/AppBundle/Controller/CompetitionController.php
@@ -8,15 +8,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\EntityRepository;
+use Symfony\Component\HttpFoundation\Session;
+use MatchTracker\AppBundle\Entity\Leagues;
+use MatchTracker\AppBundle\Form\LeaguesType;
+use Symfony\Component\Form\FormError;
 
+/**
+ * Competition Controller
+ */
 class CompetitionController extends Controller {
-	
-	public function indexAction($sport) {
+
+    /**
+     * Competition summary with or without filter
+     *
+     * @param $sport
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction($sport) {
 		return $this->render('MatchTrackerAppBundle:Competition:index.html.twig', 
 				array('sport' => $sport));
 	}
-	
-	
+
+    /**
+     * Detail view of a competition
+     *
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function detailAction($name) {
     	
     	$league = $this->getDoctrine()
@@ -27,32 +45,31 @@ class CompetitionController extends Controller {
         		array('league' => $league));
     }
 
+    /**
+     * Competition maker step1: general info
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function makerAction(Request $request) {
+        // Form constraints
         $constraint = new Assert\Collection(array(
             'name' => new Assert\NotBlank(array('message' => 'Gelieve naam in te vullen')),
-            'place' => new Assert\NotBlank(array('message' => 'Gelieve naam in te vullen')),
             'description' => new Assert\NotBlank(array('message' => 'Gelieve beschrijving in te vullen')),
-            'sport' => new Assert\NotBlank(array('message' => 'Gelieve een sport aan te duiden'))
+            'startdate' => array(
+                new  Assert\Date(array('message' => 'Gelieve een datum aan te duiden')),
+                new  Assert\NotBlank(array('message' => 'Gelieve een datum in te vullen'))),
+            'enddate' => array(
+                new  Assert\Date(array('message' => 'Gelieve een datum aan te duiden')),
+                new  Assert\NotBlank(array('message' => 'Gelieve een datum in te vullen'))),
         ));
 
-        // Get Sports
-        $products = $this->getDoctrine()
-            ->getRepository('MatchTrackerAppBundle:Sports')->findAll();
-
-        $productNames = array();
-
-        foreach ($products as $prod) {
-            array_push($productNames, $prod->getName());
-        }
-
+        // Create form with constraints
         $form = $this->createFormBuilder(null, array('validation_constraint' => $constraint))
-            -> add('name', 'text', array('label' => 'Naam'))
-            -> add('place', 'text', array('label' => 'Plaats'))
-            -> add('description', 'textarea', array('label' => 'Beschrijving'))
-            ->add('sport', 'choice', array(
-            'label' => 'Sport',
-            'choices'   => $productNames,
-            'empty_value' => 'Kies een sport'))
+            ->add('name', 'text', array('label' => 'Naam'))
+            ->add('description', 'textarea', array('label' => 'Beschrijving'))
+            ->add('startdate', 'date', array('widget' => 'single_text', 'label' => 'Start datum (dd-mm-jjjj)', 'format' => 'dd-MM-yyyy'))
+            ->add('enddate', 'date', array('widget' => 'single_text', 'label' => 'Eind datum (dd-mm-jjjj)', 'format' => 'dd-MM-yyyy'))
             ->getForm();
 
 
@@ -60,26 +77,106 @@ class CompetitionController extends Controller {
             $form->bind($request);
             $data = $form->getData();
 
+            // End date must by later then start date
+            if ($data['startdate'] >  $data['enddate']){
+                 $form->get('enddate')->addError(new FormError('Eind datum moet na de start datum zijn'));
+             }
 
             if ($form->isValid()) {
-                /*
+
+                // New competition
                 $competition = new Leagues();
                 $competition->setName($data["name"]);
-                $competition->setPlace($data["place"]);
                 $competition->setDescription($data["description"]);
-                $competition->setUser($this->getUser());
+                $competition->setStartdate($data['startdate']);
+                $competition->setEnddate($data['enddate']);
 
-                // perform some action, such as saving the task to the database
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($competition);
-                $em->flush();*/
+                // Put competition in session
+                $this->get('session')->set('competition', $competition);
 
-                return $this->redirect($this->generateUrl('competition_maker_details'));
+                // Redirect to step 2
+                return $this->redirect($this->generateUrl('competition_maker_details', array('sport' => 'sport')));
             }
 
         }
 
         return $this->render('MatchTrackerAppBundle:Competition:maker.html.twig', array(
             "form" => $form->createView()));
+    }
+
+    /**
+     * Competition maker step 2: detailed info
+     *
+     * @param $sport
+     * @param $type
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function makerDetailAction($sport, $type, Request $request) {
+        // Redirect if there's no competition in session
+        $competition = $this->get('session')->get('competition');
+        if ($competition === null) {
+            return $this->redirect($this->generateUrl('competition_maker'));
+        }
+
+        // Get sportTypes
+        $sportTypes = $this->getDoctrine()
+            ->getRepository('MatchTrackerAppBundle:SportTypes')->findAll();
+
+        // Create sports structuur
+        $sportTypesArray = array();
+        foreach ($sportTypes as $sportType) {
+            $sportTypesArray[$sportType->getSports()->getName()][] = $sportType->getName();
+        }
+
+        // Form will not be created until a sport type is selected
+        if ($type !== 'null') {
+            $sportType = $this->getDoctrine()
+                ->getRepository('MatchTrackerAppBundle:SportTypes')->findOneByName($type);
+
+            // Create LeaguesType form
+            $form = $this->createForm(new LeaguesType($sport, $sportType));
+
+            if ($request->isMethod('POST')) {
+                $form->bind($request);
+                $data = $form->getData();
+
+                if ($form->isValid()) {
+                    $this->get('session')->remove('competition');
+
+                    $competition->setUser($this->getUser());
+                    $competition->setSportTypes($sportType);
+                    $competition->setNumberOfTeams($data['numberOfTeams']);
+
+                    if ($sportType->getPlayersOnField() != null) {
+                        $competition->setPlayersOnField($sportType->getPlayersOnField());
+                    } else {
+                        $competition->setPlayersOnField($data['playersOnField']);
+                    }
+
+                    if ($data['location'] === 'one') {
+                        $competition->setFields($data['field']);
+                        $competition->setPlace($data['place']);
+                    }
+
+                    // perform some action, such as saving the task to the database
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($competition);
+                    $em->flush();
+
+                    //return $this->redirect($this->generateUrl('competition_maker_details'));
+                }
+            }
+
+        }
+
+        // Items to render with template
+        $toRender = array('sportTypes' => $sportTypesArray,
+            'sport' => $sport,
+            'type' => $type);
+        if ($type !== 'null') {
+            $toRender['form'] = $form->createView();
+        }
+        return $this->render('MatchTrackerAppBundle:Competition:maker_details.html.twig', $toRender);
     }
 }
