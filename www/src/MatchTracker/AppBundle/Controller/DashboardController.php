6f89e14e7ddb305b8dceef11ee72dd7bc37f40a5
Stéphane Poppe <stephanepoppe@gmail.com>
2012-11-07 22:44:11 +0100

    dashboard
    Signed-off-by: Stéphane Poppe <stephanepoppe@gmail.com>

--- www/src/MatchTracker/AppBundle/Controller/DashboardController.php
+++ www/src/MatchTracker/AppBundle/Controller/DashboardController.php
@@ -35,7 +35,8 @@ class DashboardController extends Controller {
 
 
     public function settingsAction(){
-        return $this->render('MatchTrackerAppBundle:Dashboard:profile.html.twig');
+        return $this->render('MatchTrackerAppBundle:Profile:edit.html.twig');
+        //return $this->render('MatchTrackerAppBundle:Dashboard:profile.html.twig');
     }
 	
 	
