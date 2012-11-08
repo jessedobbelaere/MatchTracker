b61073d761e415cec93c46829e5d056e39fb7226
Jesse Dobbelaere <jesse@dobbelaere-ae.be>
2012-11-08 02:11:05 +0100

    Custom facebook button + bugfixes
    * Custom facebook button javascript function
    * CSS bugfixes
    * Added smooth "Go to top" function at bottomlogo
    * Added favicon's
    * Facebooklogin redirects to dashboard
    * Code cleanup
    
    Signed-off-by: Jesse Dobbelaere <jesse@dobbelaere-ae.be>

---
+++ www/web/bundles/matchtrackerapp/js/scripts.js
@@ -0,0 +1,16 @@
+/*
+ * MatchTracker scripts
+ * http://www.matchtracker.be
+ *
+ */
+
+$(document).ready(function(){
+
+
+	/* SMOOTH GO TO TOP */
+	$('#goTop').click(function(){
+		$("html, body").animate({ scrollTop: 0 }, 600);
+		return false;
+	});
+
+});
