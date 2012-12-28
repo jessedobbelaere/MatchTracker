2959e74ac4d3fab9b48367a9f940b4a49877cf49
Jesse Dobbelaere <jesse@dobbelaere-ae.be>
2012-12-28 23:39:34 +0100

    Fixed merge conflict
    Signed-off-by: Jesse Dobbelaere <jesse@dobbelaere-ae.be>

--- www/src/MatchTracker/Bundle/AppBundle/Controller/TeamController.php
+++ www/src/MatchTracker/Bundle/AppBundle/Controller/TeamController.php
@@ -3,6 +3,10 @@
 namespace MatchTracker\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class TeamController extends Controller {
 
@@ -20,7 +24,7 @@ class TeamController extends Controller {
 	 *
 	 * @return \Symfony\Component\HttpFoundation\Response
 	 */
-	public function editAction($nameCanonical, $code) {
+	public function editAction($nameCanonical, $code, Request $request) {
 
 		/**
 		 * Team object
@@ -28,11 +32,44 @@ class TeamController extends Controller {
 		 */
 		$team = $this->getDoctrine()
 			->getRepository('MatchTrackerAppBundle:Teams')
-			->findOneByNameCanonical($nameCanonical);
+			->findOneBy(array('nameCanonical' => $nameCanonical));
 
+		// Security check
+		if($team == null || ($code !== $team->getCode())) {
+			return $this->redirect($this->generateUrl('homepage'));
+		}
 
+		// Create form with constraints
+		$form = $this->createFormBuilder(null)
+			->add('name', 'text', array('label' => 'Naam', 'label_attr' => array('class' => 'control-label'), 'attr' => array('placeholder' => 'Naam'), 'data' => $team->getName()))
+			->getForm();
 
-		return $this->render('MatchTrackerAppBundle:Team:edit.html.twig');
+		if ($request->isMethod('POST')) {
+			$form->bind($request);
+			$data = $form->getData();
+
+			// Form is valid?
+			if ($form->isValid()) {
+				$team->setName($data['name']);
+				$team->generateNameCanonical();
+
+				// Save changes
+				$em = $this->getDoctrine()->getManager();
+				$em->persist($team);
+				$em->flush();
+
+				// Redirect to new canonical url
+				return $this->redirect($this->generateUrl('team_edit', array('nameCanonical' => $team->getNameCanonical(), 'code' => $team->getCode())));
+			}
+
+		}
+
+		// Render view
+		return $this->render('MatchTrackerAppBundle:Team:edit.html.twig',
+			array(
+				'team' => $team,
+				'form' => $form->createView()
+			));
 	}
 
 }
