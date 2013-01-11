73b936cc35c1ecb34a8a5181946dbda09e1d3254
Stéphane Poppe <stephanepoppe@gmail.com>
2013-01-11 04:50:09 +0100

    matchController
    Signed-off-by: Stéphane Poppe <stephanepoppe@gmail.com>

---
+++ www/src/MatchTracker/Bundle/AppBundle/Controller/MatchController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: stephane
+ * Date: 1/01/13
+ * Time: 11:28
+ * To change this template use File | Settings | File Templates.
+ */
+class MatchController
+{
+
+}
