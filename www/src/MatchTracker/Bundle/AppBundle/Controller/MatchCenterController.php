e5643708ff6b6ee9ce64ee30c69c743eac39f381
bert.beeckman <bert.beeckman@kahosl.be>
2013-01-11 14:24:56 +0100

    MatchCenter: report

--- www/src/MatchTracker/Bundle/AppBundle/Controller/MatchCenterController.php
+++ www/src/MatchTracker/Bundle/AppBundle/Controller/MatchCenterController.php
@@ -3,6 +3,10 @@
 namespace MatchTracker\Bundle\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class MatchCenterController extends Controller {
 
@@ -46,33 +50,76 @@ class MatchCenterController extends Controller {
         );
     }
 
-    public function reportAction($id, $request) {
+    public function reportAction($id, Request $request) {
         // fetch the competitions related to the user
-       /*
+
         $match = $this->getDoctrine()
             ->getRepository('MatchTrackerAppBundle:Matches')
             ->find($id);
 
-        $MatchEvents = $this->getDoctrine()
-            ->getRepository('MatchTrackerAppBundle:MatchesHasMatchEvents')
-            ->findBy(array('matches' => $id), array('time' => 'desc'));
 
-        $builder = $this->createFormBuilder($MatchEvents);
-        $form = $builder->add('event', 'collection', array(
-            'label' => 'Events',
-            'label_attr' => array('class' => 'control-label'),
-            'type' => new TeamsType(),
-            'allow_add' => true,
-            'allow_delete' => true,
-            'by_reference' => false
-        ))->getForm();
+        $matchEvents = $this->getDoctrine()
+            ->getRepository('MatchTrackerAppBundle:MatchEvents')
+            ->findAll();
+
+        $playersArray = array();
+
+        foreach ($match->getHomeTeam()->getPlayers() as $player) {
+            $playersArray[$player->getId()] = ($player->getNumber() . ': ' . $player->getName());
+        }
+
+        foreach ($match->getAwayTeam()->getPlayers() as $player) {
+            $playersArray[$player->getId()] = ($player->getNumber() . ': ' . $player->getName());
+        }
+
+        $constraint = new Assert\Collection(array(
+            'player' => array(
+                new Assert\NotBlank(array('message' => 'Gelieve aantal velden in te vullen'))),
+            'event' => array(
+                new Assert\NotBlank(array('message' => 'Gelieve aantal velden in te vullen'))),
+            'time' => array(
+                new Assert\NotBlank(array('message' => 'Gelieve aantal velden in te vullen')))
+        ));
+
+        $eventArray = array();
+        foreach ($matchEvents as $event) {
+            $eventArray[$event->getId()] = $event->getName();
+        }
+
+        $form = $this->createFormBuilder(null, array('validation_constraint' => $constraint))
+            ->add('player', 'choice', array('choices' => $playersArray,'label' => 'Speler'))
+            ->add('event', 'choice', array('choices' => $eventArray,'label' => 'Actie'))
+            ->add('time', 'integer', array('label' => 'Tijd',))
+            ->getForm();
 
         if ($request->isMethod('POST')) {
             $form->bind($request);
+            $data = $form->getData();
 
             if ($form->isValid()) {
 
+                $matchEvents = $this->getDoctrine()
+                    ->getRepository('MatchTrackerAppBundle:MatchEvents')
+                    ->find($data['event']);
 
+                $player = $this->getDoctrine()
+                    ->getRepository('MatchTrackerAppBundle:Players')
+                    ->find($data['player']);
+
+                $teams = $player->getTeams();
+
+
+                $matchHasEvent = new \MatchTracker\Bundle\AppBundle\Entity\MatchesHasMatchEvents();
+
+                $matchHasEvent->setTime($data['time']);
+                $matchHasEvent->setMatchEvents($matchEvents);
+                $matchHasEvent->setMatches($match);
+                $matchHasEvent->setPlayers($player);
+                $matchHasEvent->getTeams($teams[0]);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($matchHasEvent);
+                $em->flush();
             }
 
         }
@@ -80,8 +127,8 @@ class MatchCenterController extends Controller {
 
         return $this->render('MatchTrackerAppBundle:MatchCenter:report.html.twig',
             array(
-                'form' => $form
+                'form' => $form->createView()
             )
-        );*/
+        );
     }
 }
